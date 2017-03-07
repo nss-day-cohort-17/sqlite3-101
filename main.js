797aa64f5f8f02998bc9fe86f60d511c5b25d5f7
Dominic Serrano <dmsrojo@gmail.com>
2017-03-07 10:38:50 -0600

    Now dynamically populating table

--- main.js
+++ main.js
@@ -8,6 +8,24 @@ const dropEmployees = () => {
 };
 // dropEmployees();
 
-db.run("CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary NUMBER(6,2))");
+// Creates the employees table if it does not exist
+// Will not execute if the table exists
+db.run("CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary INT, department TEXT)");
 
-// db.run(`INSERT INTO employees VALUES (1, "Ashley", "Irwin", 5)`);
+
+const populateEmployees = () => {
+
+  const { list } = require('./employees.json');
+
+  list.forEach(each => {
+    db.run(`INSERT INTO employees VALUES (
+      ${each.id},
+      "${each.firstName}",
+      "${each.lastName}",
+      ${each.salary},
+      "${each.dept}"
+    )`);
+  })
+
+};
+// populateEmployees();
