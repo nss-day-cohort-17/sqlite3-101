109eab53cf2bc1165b62268fbe320c573b8ec07a
Dominic Serrano <dmsrojo@gmail.com>
2017-03-07 10:56:00 -0600

    Using db.all to query our employees table

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const dropEmployees = () => {
 db.run("CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary INT, department TEXT)");
 
 
+// Populates employees table with employees.json
 const populateEmployees = () => {
 
   const { list } = require('./employees.json');
@@ -29,3 +30,19 @@ const populateEmployees = () => {
 
 };
 // populateEmployees();
+
+// Boo - not good
+// db.get(`SELECT * FROM employees`, (err, row) => {
+//   console.log(row);
+// });
+
+
+db.all(`SELECT * FROM employees`, (err, allRows) => {
+  allRows.forEach(( { id, first, last, department, salary } ) => {
+    console.log(`
+      ${id} ${first} ${last}
+      from ${department} Department.
+      Salary: ${salary}
+    `);
+  });
+});
